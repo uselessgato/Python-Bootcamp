52693db19be40c8455ab825eb14988317534b628
uselessgato <felixken18@gmail.com>
2018-01-24 11:22:17 +0800

    Saving

--- collatz.py
+++ collatz.py
@@ -1,19 +1,19 @@
 def collatz(number):
     if number == 1:
-        print '1'
+        print('1')
         return 1
     elif number % 2 == 0:
-        print number/2
+        print(number/2)
         return number/2
     else:
-        print 3*number+1
+        print(3*number+1)
         return 3*number+1
         
 def execute():
     while True:
         number = input('What is your number?: ')
         try:
-            int(number)
+            number = int(number)
         except:
             print('Please provide an integer.')
             continue
