c70d911f38171e739e5accabd816c4709473a4bf
uselessgato <33298982+uselessgato@users.noreply.github.com>
2018-02-22 14:50:42 +0800

    Updated automate_ch4_comma.py to a function structure

--- automate_ch4_comma.py
+++ automate_ch4_comma.py
@@ -1,10 +1,12 @@
+def convert(list):
+    output=''
+    for i in range(0,len(spam)):
+        if i == len(spam)-1:
+            output+='and '+spam[-1]
+        else:
+            output += spam[i] + ', '
+    return output
+
 spam =['apples','bananas','tofu','cats','horses']
 
-output=''
-for i in range(0,len(spam)):
-    if i == len(spam)-1:
-        output += 'and '+spam[-1]
-    else:
-        output += spam[i]+', '
-
-print(output)
+convert(spam)
